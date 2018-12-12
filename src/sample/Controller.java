7242076b523423ad32160357f176f6352c7a47fc
BT_1N3_05 <jhoang6606@bths.edu>
2018-12-12 09:40:19 -0500

    b

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,33 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+
 public class Controller {
+    public Label helloWorld;
+    public Label Score;
+    private int count = 0;
+
+    public void sayHelloWorld(ActionEvent actionEvent)
+    {
+        helloWorld.setText("Hello World!");
+    }
+
+    public void Score1(ActionEvent actionEvent)
+    {
+        count++;
+        Score.setText(String.valueOf(count));
+    }
+
+    public void Score2(ActionEvent actionEvent)
+    {
+        count= count + 2;
+        Score.setText(String.valueOf(count));
+    }
+
+    public void Score3(ActionEvent actionEvent)
+    {
+        count=count+3;
+        Score.setText(String.valueOf(count));
+    }
 }
